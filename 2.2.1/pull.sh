3d6611429d341e2116817742249747d05f48ef0f
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2020-10-28 02:11:03 -0400

    Initial untested commit

---
+++ 2.2.1/pull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v singularity)" ]; then
+	module load singularity/3.5.3
+fi
+
+singularity pull https://depot.galaxyproject.org/singularity/hisat2%3A2.2.1--he1b5a44_2
+mv hisat2%3A2.2.1--he1b5a44_2 singularity-hisat2-2.2.1.sif
